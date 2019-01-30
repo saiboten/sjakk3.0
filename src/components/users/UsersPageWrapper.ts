63b31d250c71ab53ab5cf552066fa5e6582a97f8
saiboten <saiboten@gmail.com>
2019-01-30 22:24:26 +0100

    All connects are now with strong types - hurray

--- src/components/users/UsersPageWrapper.ts
+++ src/components/users/UsersPageWrapper.ts
@@ -1,10 +1,14 @@
 import { connect } from "react-redux";
 
 import UsersPage from "./UsersPage";
+import { AppState } from "../../types";
 
 export const UserPageWrapper = connect(
-  ({ users }: { users: any }) => ({
-    users
-  }),
+  (state: AppState) => {
+    const { users } = state;
+    return {
+      users
+    };
+  },
   dispatch => ({})
 )(UsersPage);
