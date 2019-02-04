8adedf70c3a4243cf696633705a375450bef1d12
saiboten <saiboten@gmail.com>
2019-02-04 22:54:29 +0100

    Added simple chart for keeping track of rating

--- src/components/users/UserStatistics.tsx
+++ src/components/users/UserStatistics.tsx
@@ -4,6 +4,7 @@ import UserMatchesList from "./UserMatchesList";
 import { Match, User, AppState } from "../../types";
 import { RouteComponentProps } from "react-router";
 import { StyledContainer } from "../styled/StyledContainer";
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from "recharts";
 
 interface MatchParams {
   id: string;
@@ -21,12 +22,11 @@ interface State {}
 class UserStatistics extends React.Component<Props, State> {
   render() {
     const { matches, tournaments, users, match, user } = this.props;
+    const userId = match.params.id;
 
-    if (!user) {
+    if (!user || !matches || !users[userId] || !users[userId].matches) {
       return <React.Fragment />;
     }
-
-    const userId = match.params.id;
 
     let matchesPlayed = 0;
     let wins = 0;
@@ -35,53 +35,88 @@ class UserStatistics extends React.Component<Props, State> {
     let winsAsWhite = 0;
     let winsAsBlack = 0;
 
-    if (matches && users && users[userId] && users[userId].matches) {
-      let userMatches = users[userId].matches.map((onematch: string) => {
-        return matches[onematch];
-      });
+    let userMatches = users[userId].matches.map((onematch: string) => {
+      return matches[onematch];
+    });
 
-      userMatches = userMatches.filter((xyz: Match) => xyz && xyz.completed);
+    userMatches = userMatches.filter((xyz: Match) => xyz && xyz.completed);
 
-      wins = userMatches.reduce((wonMatches: number, oneMatch: Match) => {
-        let won = 0;
-        if (oneMatch.white === userId && oneMatch.whiteWon) {
-          won = 1;
-        } else if (oneMatch.black === userId && oneMatch.blackWon) {
-          won = 1;
-        }
-        return wonMatches + won;
-      }, 0);
+    wins = userMatches.reduce((wonMatches: number, oneMatch: Match) => {
+      let won = 0;
+      if (oneMatch.white === userId && oneMatch.whiteWon) {
+        won = 1;
+      } else if (oneMatch.black === userId && oneMatch.blackWon) {
+        won = 1;
+      }
+      return wonMatches + won;
+    }, 0);
 
-      losses = userMatches.reduce((lostMatches: number, oneMatch: Match) => {
-        let lost = 0;
-        if (oneMatch.white === userId && oneMatch.blackWon) {
-          lost = 1;
-        } else if (oneMatch.black === userId && oneMatch.whiteWon) {
-          lost = 1;
-        }
-        return lostMatches + lost;
-      }, 0);
+    losses = userMatches.reduce((lostMatches: number, oneMatch: Match) => {
+      let lost = 0;
+      if (oneMatch.white === userId && oneMatch.blackWon) {
+        lost = 1;
+      } else if (oneMatch.black === userId && oneMatch.whiteWon) {
+        lost = 1;
+      }
+      return lostMatches + lost;
+    }, 0);
 
-      ties = userMatches.reduce((tiedMatches: number, oneMatch: Match) => tiedMatches + (oneMatch.remis ? 1 : 0), 0);
+    ties = userMatches.reduce((tiedMatches: number, oneMatch: Match) => tiedMatches + (oneMatch.remis ? 1 : 0), 0);
 
-      winsAsBlack = userMatches.reduce(
-        (gamesTotal: number, oneMatch: Match) => gamesTotal + (oneMatch.black === userId && oneMatch.blackWon ? 1 : 0),
-        0
-      );
+    winsAsBlack = userMatches.reduce(
+      (gamesTotal: number, oneMatch: Match) => gamesTotal + (oneMatch.black === userId && oneMatch.blackWon ? 1 : 0),
+      0
+    );
 
-      winsAsWhite = userMatches.reduce(
-        (gamesTotal: number, oneMatch: Match) => gamesTotal + (oneMatch.white === userId && oneMatch.whiteWon ? 1 : 0),
-        0
-      );
+    winsAsWhite = userMatches.reduce(
+      (gamesTotal: number, oneMatch: Match) => gamesTotal + (oneMatch.white === userId && oneMatch.whiteWon ? 1 : 0),
+      0
+    );
 
-      matchesPlayed = userMatches.length;
-    }
+    matchesPlayed = userMatches.length;
+
+    const chartData = userMatches.map((match: Match, index: number) => ({
+      name: `${index}`,
+      a:
+        match.white === userId
+          ? match.whiteInitialRating + match.whiteRatingChange
+          : match.blackInitialRating + match.blackRatingChange
+    }));
+
+    const minRating = userMatches.reduce(
+      (lowest: number, match: Match) =>
+        match.white === userId
+          ? Math.min(lowest, match.whiteInitialRating + match.whiteRatingChange)
+          : Math.min(lowest, match.blackInitialRating + match.blackRatingChange),
+      Number.MAX_SAFE_INTEGER
+    );
+
+    const maxRating = userMatches.reduce(
+      (highest: number, match: Match) =>
+        match.white === userId
+          ? Math.max(highest, match.whiteInitialRating + match.whiteRatingChange)
+          : Math.max(highest, match.blackInitialRating + match.blackRatingChange),
+      Number.MIN_SAFE_INTEGER
+    );
 
     return (
       <StyledContainer>
         <h1>Brukerstatistikk for {user.name}</h1>
+
+        <LineChart width={730} height={250} data={chartData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis />
+          <YAxis dataKey="a" type="number" domain={["dataMin - 100", "dataMax + 100"]} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="a" stroke="#8884d8" />
+        </LineChart>
+
         <ul className="flex-column">
           <li>Rating: {user.rating}</li>
+          <li>Høyeste rating {minRating}</li>
+          <li>Laveste rating: {maxRating}</li>
+          <br />
           <li>Antall kamper: {matchesPlayed}</li>
           <li>Seire: {wins}</li>
           <li>Tap: {losses}</li>
--- src/components/users/UsersPage.tsx
+++ src/components/users/UsersPage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-
 import ScoreBoard from "./ScoreBoard";
 import AddUserForm from "./AddUserForm";
 import { AppState } from "../../types";
